ac59754bc7fa94ef9e4ac5246c09370ede990fd3
CAREFREE-VILLAGER <as314202@gmail.com>
2024-08-08 19:43:40 +0530

    making some changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,11 +2,42 @@ import React from 'react';
 import './Projects.css';
 import project1 from './project1.PNG';
 import project2 from './project2.PNG';
+import project3 from './project3.PNG';
 
 const Projects = () => {
   return (
     <section id='projects'>
       <p className='project-title'>My Projects</p>
+
+      <div className="container">
+      
+        <div className="image">
+          <img src={project3} alt="Project 3" />
+        </div>
+        
+        <div className="text">
+        <p className=''>Reliane Digital Website Clone :</p>
+          <ul>
+            <li>User Authentication: Secure user login to access personalized features.</li>
+            <li> Provide an intuitive registration process for new users to create accounts</li>
+            <li>Implement backend functionalities to fetch data seamlessly from the server</li>
+            <li>FullStack Personal Project with fully functional frontend and backend</li>
+          </ul>
+          <div className="links">
+            <button className="button-link" onClick={() => window.open("https://relianceclone-frontend.vercel.app/", "_blank")}>
+              Visit Website
+            </button>
+            <button className="button-link" onClick={() => window.open("https://github.com/CAREFREE-VILLAGER/Airgarage-clone-by-Carefree/tree/main", "_blank")}>
+              GitHub Repo
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
       <div className="container">
       
         <div className="image">
